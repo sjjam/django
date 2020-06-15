5d56a8fd42a7b81aee980308309d95676f7c05fa
sjjam <shimjm21@gmail.com>
2020-06-15 10:29:46 +0900

    06/15 appname 추가

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name="pages"
+
 urlpatterns = [
     path('index/', views.index, name="index"),
     path('food/<str:name>/', views.food, name="food"),
--- mysite/secondapp/urls.py
+++ mysite/secondapp/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name="secondapp"
+
 urlpatterns = [
     path('index/', views.index, name="index"),
     path('static_example/', views.static_example, name="static_example"),
